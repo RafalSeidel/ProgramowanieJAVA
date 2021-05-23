bf1d42ab9e8d14f0354248e33215e1f3ee7beee3
RafalSeidel <rafal.seidel1@gmail.com>
2021-05-23 17:07:33 +0200

    creategitignore

---
+++ src/com/company/Human.java
@@ -0,0 +1,18 @@
+package pl.gda.wsb;
+
+public class Human {
+
+    String firstName;
+    String lastName;
+    String position;
+
+    public Human(String firstName, String lastName, String position) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+    }
+
+    public Human() {
+    }
+
+}
